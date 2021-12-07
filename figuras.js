d6d466cc23901e7a18f2e3a6e409f9faa7307663
Memo <memomanuel98@gmail.com>
2021-12-06 22:16:43 -0500

    Clase numero cuatro del curso

--- figuras.js
+++ figuras.js
@@ -71,3 +71,21 @@ function areaCirculo(radio){
 //console.log("El area del circulo mide "+ areaCirculo + "Cm");
 
 console.groupEnd();
+
+
+
+function calcularPerimetroCuadrado(){
+    const input = document.getElementById("InputCuadrado");
+    const value= input.value;
+
+    const perimetro= perimetroCuadrado(value);
+    alert(perimetro);
+
+}
+function calcularAreaCuadrado(){
+    const input = document.getElementById("InputCuadrado");
+    const value= input.value;
+
+    const area= areaCuadrada(value);
+    alert(area);
+}
